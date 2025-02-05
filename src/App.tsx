1b6378acdd3d57cdbc3a74c462e51db3e628762a
Xin Qiao <qiaoxin136@gmail.com>
2025-02-05 15:26:27 -0500

    add storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import { useEffect, useState} from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import React from 'react';
 import { uploadData } from 'aws-amplify/storage';
 
-const client = generateClient<Schema>();
-
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  const [file, setFile] = useState();
+  const [file, setFile] = React.useState();
 
   const handleChange = (event) => {
     setFile(event.target.files?.[0]);
   };
 
   const handleClick = () => {
-    console.log(file);
     if (!file) {
       return;
     }
@@ -25,30 +18,11 @@ function App() {
     });
   };
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
+    <div>
       <input type="file" onChange={handleChange} />
       <button onClick={handleClick}>Upload</button>
     </div>
-    </main>
   );
 }
 
