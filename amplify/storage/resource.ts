9f87cd8483f475c495331949aeda9f86d70a3469
Xin Qiao <qiaoxin136@gmail.com>
2025-02-05 15:10:44 -0500

    add storage

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -14,16 +14,3 @@ export const storage = defineStorage({
   })
 });
 
-export const firstBucket = defineStorage({
-  name: 'firstBucket',
-  isDefault: true, // identify your default storage bucket (required)
-});
-
-export const secondBucket = defineStorage({
-  name: 'secondBucket',
-  access: (allow) => ({
-    'private/{entity_id}/*': [
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ]
-  })
-})
